6d5c4e061d2f5561a3ebdb031e0eb6329ebbcbc8
potterbroom <149684021+potterbroom@users.noreply.github.com>
2023-11-14 20:29:58 +0800

    Update and rename zuoye4.cpp to 作业4.cpp

--- 作业4.cpp
+++ 作业4.cpp
@@ -6,11 +6,11 @@ int main()
 	unsigned int testUnint = 65534;//oxfffe
 	cout << "output in unsigned int 1 type:" << testUnint<< endl;//<<oct;
 	cout << "output in char type:!" << static_cast<char>(testUnint) << endl;
-	cout << "output in short type:" << static_cast<short>(testUnint) << endl;//Ϊʲô���Ϊ-2?:
+	cout << "output in short type:" << static_cast<short>(testUnint) << endl;//ÎªÊ²Ã´½á¹ûÎª-2?:
 	cout << "output in int type:" << static_cast<int>(testUnint) << endl;
 	cout << "output in double type:" << static_cast<double>(testUnint) << endl;
 	cout << "output in double type:" << setprecision(4) << static_cast<double>(testUnint) << endl;
-	cout << "output in Hex unsigned int type:" << oct << testUnint << endl; //8�������
+	cout << "output in Hex unsigned int type:" << oct << testUnint << endl; //8½øÖÆÊä³ö
 	system("pause");
 	return 0;
 }
